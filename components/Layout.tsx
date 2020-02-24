04d9159c724fcf5d14e2541d3441823a582a13ce
yatemmma <yatemmma@gmail.com>
2020-02-24 22:52:21 +0900

    wip

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -2,7 +2,8 @@ const layoutStyle = {
   backgroundColor: '#333',
   color: 'white',
   'line-height': '36px',
-  margin: '0px'
+  margin: '0px',
+  fontFamily: "‘San Francisco’, ‘Helvetica Neue’, HelveticaNeue, ‘Segoe UI’, Helvetica, ’游ゴシック Medium’, ‘Yu Gothic Medium’, ‘游ゴシック’, ‘Yu Gothic’, ‘游ゴシック体’, YuGothic, ‘Hiragino Kaku Gothic ProN’, HiraKakuProN-W3, ‘Noto Sans JP’, sans-serif;"
 }
   
 const Layout = props => (
